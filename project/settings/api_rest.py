22c0c5c9b75ccaa76c4bf00a0595ad52aaa6dd16
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-11 11:30:33 -0300

    Add SimpleJWT configuration and update requirements for enhanced authentication

--- project/settings/api_rest.py
+++ project/settings/api_rest.py
@@ -1,7 +1,22 @@
+from datetime import timedelta
+import os
+
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
      'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    
+    "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', 'INSECURE'),
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
