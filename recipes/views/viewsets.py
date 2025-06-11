066b5a1b924de73003221f86899a9792fabe79eb
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-11 09:58:58 -0300

    Implement category filtering in RecipesApiV2ViewSet for enhanced queryset handling

--- recipes/views/viewsets.py
+++ recipes/views/viewsets.py
@@ -12,3 +12,14 @@ class RecipesApiV2ViewSet(ModelViewSet):
     ).all()
 
     serializer_class = RecipesSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+    
