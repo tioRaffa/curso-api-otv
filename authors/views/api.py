d8b7ae965f4a8fd94a67978766bfb80e5a7f72d8
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-12 11:32:54 -0300

    Add 'me' action to AuthorApiViewSet for retrieving current user details

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from ..serializer import AuthorSerializer
 from django.contrib.auth import get_user_model
@@ -15,3 +17,16 @@ class AuthorApiViewSet(ReadOnlyModelViewSet):
         )
         
         return queryset
+    
+    @action(
+            methods=['get'],
+            detail=False #              TRUE -> Quando for |PrimaryKey|
+    )
+    def me(self, request, *args, **kwargs):
+        user = self.get_queryset().first()
+        
+        serializer = self.get_serializer(
+            instance=user
+        )
+        return Response(serializer.data)
+
