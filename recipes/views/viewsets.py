48aba363c592f88691781f52dc8ad98bbdf4ecd5
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-12 09:54:29 -0300

    Refactor RecipesApiV2ViewSet to improve queryset filtering and add permission checks

--- recipes/views/viewsets.py
+++ recipes/views/viewsets.py
@@ -1,25 +1,44 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from ..models import Recipe
 from ..serializer import RecipesSerializer
 
+from ..permissions import IsOwnerOrReadOnly
+from django.shortcuts import get_object_or_404
 
 class RecipesApiV2ViewSet(ModelViewSet):
+
     queryset = Recipe.objects.select_related(
         'author', 'category'
-    ).prefetch_related(
+        ).prefetch_related(
         'tags'
-    ).all()
+        ).all()
 
     serializer_class = RecipesSerializer
+    permission_classes = [IsOwnerOrReadOnly, ]
+
 
     def get_queryset(self):
         qs = super().get_queryset()
 
-        category_id = self.request.query_params.get('category_id', '')
+        uri_category_id = self.request.query_params.get('category_id', '')
 
-        if category_id != '' and category_id.isnumeric():
-            qs = qs.filter(category_id=category_id)
-
+        if uri_category_id != '' and uri_category_id.isnumeric():
+            qs = qs.filter(category_id=uri_category_id)
         return qs
     
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk
+        )
+        self.check_object_permissions(self.request, obj)
+        return obj
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        return super().perform_create(serializer)    
